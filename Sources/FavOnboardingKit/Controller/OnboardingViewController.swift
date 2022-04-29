a0e9abd29604603be01fde02587d46455ea631f3
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-04-29 15:56:17 +0530

    add 🚀 complete functionality of walkthrough

---
+++ Sources/FavOnboardingKit/Controller/OnboardingViewController.swift
@@ -0,0 +1,96 @@
+//
+//  OnboardingViewController.swift
+//  
+//
+//  Created by Naman Vaishnav on 29/04/22.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingViewController: UIViewController {
+    
+    var nextButtonDidTap: ((Int) -> Void)?
+    var getStartedButtonDidTap: (() -> Void)?
+    
+    private let slides: [Slide]
+    private let tintColor: UIColor
+    private let themeFont: UIFont
+    
+    private lazy var transitionView: TransitionView = {
+        let view = TransitionView(slides: slides, tintColor: tintColor, themeFont: themeFont)
+        return view
+    }()
+    
+    private lazy var buttonContainerView: ButtonContainerView = {
+        let view = ButtonContainerView(tintColor: tintColor)
+        view.nextButtonDidTap = { [weak self] in
+            guard let this = self else { return }
+            this.nextButtonDidTap?(this.transitionView.slideIndex)
+            this.transitionView.handleTap(direction: .right)
+        }
+        view.getStartedButtonDidTap = getStartedButtonDidTap
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [transitionView, buttonContainerView])
+        view.axis = .vertical
+        return view
+    }()
+    
+    init(slides: [Slide], tintColor: UIColor, themeFont: UIFont) {
+        self.slides = slides
+        self.tintColor = tintColor
+        self.themeFont = themeFont
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupGesture()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        transitionView.start()
+    }
+    
+    func stopAnimation() {
+        transitionView.stop()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        
+        view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view)
+        }
+        
+        buttonContainerView.snp.makeConstraints { make in
+            make.height.equalTo(120)
+        }
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewDidTap(_:)))
+        transitionView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func viewDidTap(_ tap: UITapGestureRecognizer) {
+        let point = tap.location(in: view)
+        let midPoint = view.frame.size.width / 2
+        if point.x > midPoint {
+            transitionView.handleTap(direction: .right)
+        } else {
+            transitionView.handleTap(direction: .left)
+        }
+    }
+}
---
+++ Sources/FavOnboardingKit/Model/Slide.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Naman Vaishnav on 29/04/22.
+//
+
+import Foundation
+import UIKit
+
+public struct Slide {
+    public let image: UIImage
+    public let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+}
